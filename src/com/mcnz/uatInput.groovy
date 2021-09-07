3bca6984812cf58b0c4db2a8b70ca38521b06dc3
omkar1025 <86650140+omkar1025@users.noreply.github.com>
2021-09-07 08:20:04 +0530

    Update uatInput.groovy

--- src/com/mcnz/uatInput.groovy
+++ src/com/mcnz/uatInput.groovy
@@ -3,7 +3,7 @@ package com.mcnz
 public class uatInput {
 
   def buildIsUatApproved() {
-    def file = new File("C:/_tools/approved.txt")
+    def file = new File("D:/jenkins.war")
     
     if (file.exists()){
       return true;
